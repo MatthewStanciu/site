41b111616a41a3632575e015386f82dd41b9dd8e
TechBug2012 <mattbstanciu@gmail.com>
2018-08-05 21:41:48 -0400

    added a header thing, installed some dependencies

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,21 @@
 import React from "react";
+import styled from 'styled-components'
+import Helmet from 'react-helmet'
 
-export default () => <div style={{ color: `blue` }}>Hello Gatsby!</div>;
+var background = {backgroundColor: '#000000'}
+
+export default () => (
+  <Helmet
+      title='Matthew :)'
+      meta={[
+      {
+        name: 'description',
+        content: 'Development portfolio',
+      },
+      {
+        name: 'keywords',
+        content: 'reactjs, javascript, front-end, web development',
+      },
+    ]}
+  />
+)
