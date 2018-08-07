ba2a01b56d07db773913a881cba6c41c7cf1bd7f
TechBug2012 <mattbstanciu@gmail.com>
2018-08-06 20:56:06 -0400

    moved wrapper to components file

--- src/components/components.js
+++ src/components/components.js
@@ -1,2 +1,14 @@
 import React from 'react'
-import { Heading } from 'rebass'
+import { Heading, Flex } from 'rebass'
+import {mx} from '../reactstyle'
+
+export const Wrapper = Flex.extend.attrs({
+  direction: ['column', 'row'],
+  px: 3
+})`
+  text-align: center;
+  ${mx[0]} {
+    min-height: 80vh;
+    text-align: left;
+  }
+`
