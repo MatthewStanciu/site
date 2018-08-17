dc6a597a6f57fc997dcb1b437eae27fb6746a8d0
MatthewStanciu <mattbstanciu@gmail.com>
2018-08-16 23:04:23 -0400

    changed some awkward phrases in paragraph

--- src/pages/index.js
+++ src/pages/index.js
@@ -39,7 +39,7 @@ export default () => (
           </Box>
           <Project width={350} href="https://www.instagram.com/anonbot.wl/" title="Anonbot WL" desc="An Instagram bot for students at my school to submit confessions and announce anonymously"></Project>
           <Project width={350} href="http://thewritersblock.tech" title="The Writer's Block" desc="A decentralized writing platform that allows users to easily reward their favorite authors with Ethereum. Made at Hack Chicago 2018"></Project>
-          <ProjectFinish width={350} href="https://sideb-textgen.glitch.me" title="Side B Text Gen" desc="Pretty title and block quote generator to give a nice touch to Medium articles"></ProjectFinish>
+          <ProjectFinish width={350} href="https://sideb-textgen.glitch.me" title="Side B Text Gen" desc="A pretty title and block quote generator to give a nice touch to Medium articles"></ProjectFinish>
         </Left>
       </HeaderBlock>
     </Wrapper>
@@ -53,10 +53,10 @@ export default () => (
             Junior/Senior High School in West Lafayette, Indiana.<br/><br/>
 
             I first discovered my love for computers when I was 6 years old and my mom gave me an old laptop
-            from her work that nobody used anymore. Instead of being thrown away, it became my computer,
-            meaning that I had full access and admin priveleges. I quickly filled it with viruses and
-            eventually, the screen burnt out. Instead of fixing it, my mom found two more old laptops
-            and gave them to me. I eventually broke those as well. This was such an important time of
+            from her workplace that nobody used anymore. Instead of being thrown away, it became my computer,
+            meaning that I had full access and admin priveleges. I quickly filled it with viruses, and
+            eventually, the screen burned out. Instead of fixing it, my mom found two more old laptops
+            and gave them to me. I eventually broke those as well. This was such an important time in
             my life because it showed me that I loved how computers worked and that I wanted to keep learning
             how they work. <br/><br/>
 
@@ -65,15 +65,16 @@ export default () => (
             to countless imaginative short stories. I have spent much of my life writing and playing with
             computers since then. <br/><br/>
 
-            I learned to code on Codecademy when I was 11. But I  always longed to be a part of something greater.
-            When I was almost 12, in December 2013, I started
+            I learned to code on Codecademy when I was 11, but I always longed to be a part of something bigger; to
+            do real things and facilitate real change with what I had learned.
+            So, when I was almost 12, in December 2013, I started
             my first Minecraft server called MCFun Central and began to learn how to make Minecraft server plugins.
             MCFun Central turned into <Link target="_" href="https://extrillius.wordpress.com/" color="#afaeae">The Extrillius Network</Link> in January 2015, a project that I kept going for
-            two years before ultimately shutting it down. <br/><br/>
+            two years before shutting it down. <br/><br/>
 
             I have now combined my two passions by running a <Link target="_" href="https://hackclub.com" color="red">Hack Club</Link> at my school, as well as a new school
             publication, <Link target="_" href="http://sideb.media" color="#CA4A3B">Side B</Link>.
-            I'm passionate about spreading computer science to everyone and enacting social change through community organzing. <br/><br/>
+            I'm passionate about spreading computer science to everyone and facilitating social change through community organzing. <br/><br/>
 
             I love what I do and I hope I can do great things in the future.</Paragraph>
         </HeaderBlock>
@@ -84,8 +85,8 @@ export default () => (
       <CenteredBox>
         <Box width={[350, 500]}>
           <Subhead>My Positions</Subhead>
-          <Project width={[350, 500]} href="https://wlcat.club" title="CAT Club President" desc="CAT Club is West Lafayette Jr/Sr High School's Hack Club. Students get together in a hackathon-like structure and learn to code by making amazing projects." />
-          <Project width={[350, 500]} href="http://sideb.media" title="Side B President" desc="Side B is a new club and student publication at my high school, aiming to give every student a voice and become a better writer through a strong local community and the freedom to write whatever they want."/>
+          <Project width={[350, 500]} href="https://wlcat.club" title="CAT Club President" desc="CAT Club is West Lafayette Jr/Sr High School's Hack Club. Students get together in hackathon-like meetings and learn to code by making amazing projects." />
+          <Project width={[350, 500]} href="http://sideb.media" title="Side B President" desc="Side B is a new club and student publication at my high school which aims to give every student a voice and become a better writer through a strong local community and the freedom to write whatever they want."/>
           <Project width={[350, 500]} href="https://codeday.org/indianapolis" title="CodeDay Indianapolis Regional Manager" desc="CodeDay is a fun, nationwide hackathon aimed toward beginners that strengthens local communities and gets students interested in coding. I am the regional manager for Indianapolis." />
           <ProjectFinish width={[350, 500]} href="https://altheamesh.com" title="AltheaMesh West Lafayette Ambassador" desc="Althea aims to give local communities the power to provide internet to themselves through a cryptocurrency-powered mesh network. I am in charge of deploying an Althea network in West Lafayette." />
         </Box>
@@ -132,7 +133,7 @@ export const Break = Flex.extend.attrs({
 })`
   text-align: center;
   ${mx[0]} {
-    min-height: 12vh;
+    min-height: 5vh;
     text-align: left;
   }
 `
